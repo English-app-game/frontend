69cc390f8f0ecda2fffb1dcbdfcf4a4e38b8c1b0
tomergol15 <golod505@gmail.com>
2025-04-21 20:19:10 +0300

    navigate to rooms page after clicking the exit room

--- src/ui/components/ExitButton.jsx
+++ src/ui/components/ExitButton.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const ExitButton = () => {
+  const navigate = useNavigate();
+
+  const handleExit = () => {
+    navigate('/rooms'); 
+  };
+
   return (
-   <button className='`font-sans bg-rose-300 border-4 border-orange-600 text-white font-extrabold text-lg px-8 py-3 rounded-full shadow-md hover:bg-rose-300 active:translate-y-1 active:shadow-inner
+   <button onClick={handleExit} className='`font-sans bg-rose-300 border-4 border-orange-600 text-white font-extrabold text-lg px-8 py-3 rounded-full shadow-md hover:bg-rose-300 active:translate-y-1 active:shadow-inner
     cursor-pointer transition-all duration-150'>EXIT ROOM</button>
   )
 }
