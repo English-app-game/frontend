2842c59553181386679cdfc32e29ce74c2fcf177
OrYossef <165793124+OrYossef@users.noreply.github.com>
2025-04-22 17:16:22 +0300

    home page design

--- src/ui/pages/Home.jsx
+++ src/ui/pages/Home.jsx
@@ -1,7 +1,26 @@
 import PrimaryButton from "../components/PrimaryButton";
 
 export default function Home() {
-  return <div>
-    Hello from Home
-    </div>;
+  return (
+    <div className="relative w-full h-screen overflow-hidden">
+      <img
+        src="/homePage.png"
+        alt="background"
+        className="absolute top-0 left-0 w-full h-full  "
+      />
+
+      <div className="relative z-10 flex flex-col items-center justify-center h-full ">
+        <h1 className="text-[#000080] bg-[#B2F8F5] text-5xl font-bold mb-4 ">
+          WELCOME TO
+        </h1>
+        <h1 className="text-[#000080] bg-[#B2F8F5] text-6xl font-bold mb-4">
+          SPEALISH
+        </h1>
+        <h1 className="text-blue-400 bg-[#B2F8F5] text-2xl font-bold mb-6">
+          Learn English with us!
+        </h1>
+        <PrimaryButton text="START" />
+      </div>
+    </div>
+  );
 }
