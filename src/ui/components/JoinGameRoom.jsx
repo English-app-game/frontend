5175c099f6a8acf4df2e17c6f53745602caf2119
maymichaeli <maushe55@gmail.com>
2025-04-24 12:27:21 +0300

    fix pr comments

--- src/ui/components/JoinGameRoom.jsx
+++ src/ui/components/JoinGameRoom.jsx
@@ -1,30 +1,53 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 import { useNavigate } from 'react-router-dom';
 import joinRoomIcon from "../../assets/images/joinRoomIcon.png";
 
-const JoinGameRoom = ({ index, currentPlayers, capacity }) => {
-    const navigate = useNavigate();
+const JoinGameRoom = ({ id, currentPlayers, capacity, onJoinAttempt }) => {
+  const navigate = useNavigate();
 
-    return (
-        <div className='w-45 h-auto flex flex-col items-center hover:cursor-pointer' 
-        onClick={() => navigate(`/rooms/${index}`)}
-        >
-            <p className='font-bold text-2xl'>ROOM {index + 1}</p>         {/* i need to add the {room index} acordding to the room that exist */}
-            <div className="relative w-45 h-auto">
-                <img
-                    src={joinRoomIcon}
-                    alt="Join Room Icon"
-                    className="w-full h-full object-cover rounded-lg"
-                />
-                <p className="absolute bottom-4 left-6 text-black text-[20px] font-bold">
-                {currentPlayers}/{capacity} PLAYERS
-                </p>
-            </div>
+  const handleClick = () => {
+    if (currentPlayers >= capacity) {
+      if (onJoinAttempt) {
+        onJoinAttempt({ id, full: true });
+      } else {
+        alert("This room is full!");
+      }
+      return;
+    }
 
-            {/* need to add the room capacity and how much players are in the room */}
+    if (onJoinAttempt) {
+      onJoinAttempt({ id, full: false });
+    }
 
-        </div>
-    )
-}
+    navigate(`/rooms/${id}`);
+  };
 
-export default JoinGameRoom
+  return (
+    <div
+      className='w-45 h-auto flex flex-col items-center hover:cursor-pointer'
+      onClick={handleClick}
+    >
+      <p className='font-bold text-2xl'>ROOM {id + 1}</p>
+      <div className="relative w-45 h-auto">
+        <img
+          src={joinRoomIcon}
+          alt="Join Room Icon"
+          className="w-full h-full object-cover rounded-lg"
+        />
+        <p className="absolute bottom-4 left-6 text-black text-[20px] font-bold">
+          {currentPlayers}/{capacity} PLAYERS
+        </p>
+      </div>
+    </div>
+  );
+};
+
+JoinGameRoom.propTypes = {
+  id: PropTypes.number.isRequired,
+  currentPlayers: PropTypes.number.isRequired,
+  capacity: PropTypes.number.isRequired,
+  onJoinAttempt: PropTypes.func,
+};
+
+export default JoinGameRoom;
--- src/ui/pages/ServersRoom.jsx
+++ src/ui/pages/ServersRoom.jsx
@@ -1,8 +1,9 @@
 import JoinGameRoom from "../components/JoinGameRoom";
+import PropTypes from 'prop-types';
 
 export default function ServersRoom() {
   return <div>
     ServersRoom
-    <JoinGameRoom></JoinGameRoom>
+    <JoinGameRoom id={0} currentPlayers={2} capacity={5} />
     </div>;
 }
