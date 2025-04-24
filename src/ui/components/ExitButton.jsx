82968c5a9df258e3522924e05e6260626286cf96
tomergol15 <golod505@gmail.com>
2025-04-24 17:10:18 +0300

    update exit-room component to be more generic, using proptypes

--- src/ui/components/ExitButton.jsx
+++ src/ui/components/ExitButton.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const ExitButton = () => {
-  const navigate = useNavigate();
-
-  const handleExit = () => {
-    navigate('/rooms'); 
-  };
-
+const ExitButton = ({ onClick, children }) => {
   return (
-   <button onClick={handleExit} className='`font-sans bg-rose-300 border-4 border-orange-600 text-white font-extrabold text-lg px-8 py-3 rounded-full shadow-md hover:bg-rose-300 active:translate-y-1 active:shadow-inner
-    cursor-pointer transition-all duration-150'>EXIT ROOM</button>
-  )
-}
+    <button onClick={onClick} className='font-sans bg-rose-300 border-4 border-orange-600 text-white font-extrabold text-lg px-8 py-3 rounded-full shadow-md hover:bg-rose-300 
+      active:translate-y-1 active:shadow-inner cursor-pointer transition-all duration-150'>{children}</button>
+  );
+};
 
-export default ExitButton
+ExitButton.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+export default ExitButton;
--- src/ui/pages/WaitingRoom.jsx
+++ src/ui/pages/WaitingRoom.jsx
@@ -1,8 +1,14 @@
 import ExitButton from "../components/ExitButton";
+import { useNavigate } from 'react-router-dom';
 
 export default function WaitingRoom() {
+  const navigate = useNavigate();
+
+  const handleExit = () => {
+    navigate('/rooms'); 
+  };
+
   return <div>
-    {/* WaitingRoom */}
-    <ExitButton />
+    <ExitButton onClick={handleExit}>Exit Room</ExitButton>
   </div>;
 }
