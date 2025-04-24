b39af45d766ecd50e5af7db57f95506f849a34bd
maymichaeli <maushe55@gmail.com>
2025-04-24 13:41:10 +0300

    add proptype

--- src/ui/components/BlueBox.jsx
+++ src/ui/components/BlueBox.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const BlueBox = ({ children }) => {
   return (
@@ -6,6 +7,10 @@ const BlueBox = ({ children }) => {
       {children}
     </div>
   );
+};
+
+BlueBox.propTypes = {
+  children: PropTypes.node.isRequired,
 };
 
 export default BlueBox;
