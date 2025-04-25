52e162f016b218c4101b58e01609f4b8e89efd94
LiadLati <liadl57@gmail.com>
2025-04-25 15:28:41 +0300

    avatarImg fixes

--- src/ui/components/AvatarImg.jsx
+++ src/ui/components/AvatarImg.jsx
@@ -1,8 +1,8 @@
 const AvatarImg = ({ src, alt , className}) => {
   return (
-    <div>
+    <>
       <img src={src} alt={alt} className={className} />
-    </div>
+    </>
   );
 };
 export default AvatarImg;
