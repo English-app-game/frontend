8e9a5285984327b3f9d93baa4ed8624b2cc3a81f
LiadLati <liadl57@gmail.com>
2025-04-26 14:37:25 +0300

    AvatarImg final fixes

--- src/ui/components/AvatarImg.jsx
+++ src/ui/components/AvatarImg.jsx
@@ -1,8 +1,4 @@
-const AvatarImg = ({ src, alt , className}) => {
-  return (
-    <>
-      <img src={src} alt={alt} className={className} />
-    </>
-  );
+const AvatarImg = ({ src, alt }) => {
+  return <img src={src} alt={alt} className="border-white border-2 rounded-xl transition-transform duration-300 hover:scale-110 cursor-pointer" />;
 };
 export default AvatarImg;
