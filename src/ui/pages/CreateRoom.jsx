ddea905a44ef69897976921ad2caf12e02b72a3b
LaNguAx <itayakni@gmail.com>
2025-04-28 12:45:03 +0300

    rooms page
    rooms page

--- src/ui/pages/CreateRoom.jsx
+++ src/ui/pages/CreateRoom.jsx
@@ -1,3 +1,12 @@
+import { useNavigate } from "react-router-dom";
+import PrimaryButton from "../components/PrimaryButton";
+import { ROOMS_LIST } from "../../routes/routes_consts";
+
+
 export default function CreateRoom() {
-  return <div>CreateRoom</div>;
+
+  const navigate = useNavigate();
+  return <div>CreateRoom
+    <PrimaryButton text='Back to rooms' onClick={(e) => navigate(ROOMS_LIST)} />
+  </div>;
 }
