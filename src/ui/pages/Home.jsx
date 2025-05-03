3cb0d9dd075bd210eaf4b06afa32105bc7f20e31
OrYossef <165793124+OrYossef@users.noreply.github.com>
2025-05-03 14:28:32 +0300

    Final HOME PAGE
    added functionality to btn start
    fixed according the code review

--- src/ui/pages/Home.jsx
+++ src/ui/pages/Home.jsx
@@ -1,15 +1,15 @@
 import PrimaryButton from "../components/PrimaryButton" 
+import { useNavigate } from "react-router-dom";
+
 
 export default function Home() {
-  return (
-    <div className="relative w-full h-screen overflow-hidden">
-      <img
-        src="/homePage.png"
-        alt="background"
-        className="absolute top-0 left-0 w-full h-full  "
-      />
+  const navigate = useNavigate();
 
-      <div className="relative z-10 flex flex-col items-center justify-center h-full ">
+  return (
+    <div className="bg-[url('/homePage.png')] h-screen">
+
+      <div className="relative z-10 flex flex-col items-center justify-center h-full">
+        <div className="bg-[#9CF2F9] px-8 py-6 flex flex-col items-center justify-center rounded-xl shadow-lg">
         <h1 className="text-[#000080] bg-[#9CF2F9] text-6xl font-bold mb-4 ">
           WELCOME TO
         </h1>
@@ -19,8 +19,12 @@ export default function Home() {
         <h1 className="text-blue-400 bg-[#9CF2F9] text-3xl font-bold mb-6">
           Learn English with us!
         </h1>
-        <PrimaryButton text="START" />
+        <PrimaryButton text="START"  onClick={() => navigate("/login")}/>
+        </div>
+        
       </div>
+
     </div>
+      
   );
 }
