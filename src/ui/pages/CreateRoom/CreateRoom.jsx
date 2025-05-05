c9636a57f5bbe8eb6018b8dbfd2e6bc3172f1a24
tomergol15 <golod505@gmail.com>
2025-05-06 01:20:52 +0300

    seperate the createroom to peices.

---
+++ src/ui/pages/CreateRoom/CreateRoom.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { ROUTES } from "../../../routes/routes_consts";
+import LevelSelector from "./LevelSelector";
+import StatusSelector from "./StatusSelector";
+import PrimaryButton from "../../../ui/components/PrimaryButton";
+import BlueBox from "../../../ui/components/BlueBox";
+
+const CreateRoom = () => {
+  const [level, setLevel] = useState(null);
+  const [status, setStatus] = useState(null);
+  const navigate = useNavigate();
+
+  const handleCreateRoom = () => {
+    if (!level || !status) {
+      alert("Please select both level and status");
+      return;
+    }
+    navigate(ROUTES.WAITING_ROOM(), { state: { level, status } });
+  };
+
+  return (
+    <div className="bg-[url('/homePage.png')] bg-cover min-h-screen flex items-center justify-center">
+      <BlueBox size="large" className="text-center !bg-[#9CF2F9] w-[50rem] h-[30rem]">
+        <h1 className="text-4xl font-extrabold text-[#068E9E] mb-6 uppercase">CREATE YOUR GAME ROOM</h1>
+        <LevelSelector level={level} setLevel={setLevel} />
+        <StatusSelector status={status} setStatus={setStatus} />
+        <PrimaryButton text="LET'S GO" onClick={handleCreateRoom} className="bg-green-400" />
+      </BlueBox>
+    </div>
+  );
+};
+
+export default CreateRoom;
