b2bd853b8d90ee1a4fa62efc7b9a0cd3d27d95ce
maymichaeli <maushe55@gmail.com>
2025-05-11 23:17:35 +0300

    login logic and fix style in textbutton

--- src/ui/pages/Login.jsx
+++ src/ui/pages/Login.jsx
@@ -2,11 +2,11 @@ import BlueBox from "../components/BlueBox";
 import Header from "../components/Header";
 import InputField from "../components/InputField";
 import PrimaryButton from "../components/PrimaryButton";
-import TextBottom from "../components/TextButton"
+import TextBottom from "../components/TextButton";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { loginUser } from "../../services/auth.js";
-import { ROOMS_LIST } from "../../routes/routes_consts.js"
+import { ROOMS_LIST, REGISTER, LOGIN_GUEST } from "../../routes/routes_consts";
 
 export default function Login() {
   const navigate = useNavigate();
@@ -22,7 +22,6 @@ export default function Login() {
     return regex.test(email);
   };
 
-
   const handleLogin = async () => {
     let hasError = false;
 
@@ -30,19 +29,20 @@ export default function Login() {
       setEmailError("Invalid email");
       return;
     }
+
     if (!password) {
-      setPasswordError("Password is required")
+      setPasswordError("Password is required");
       hasError = true;
-    }
-    else {
+    } else {
       setEmailError("");
     }
+
     if (hasError) return;
 
     try {
-      const data = await loginUser(email, password);
-    
-      if (data.error) {
+      const { ok, data } = await loginUser(email, password);
+
+      if (!ok) {
         if (data.error === "Invalid password") {
           setPasswordError("Incorrect password");
         } else if (data.error === "User not found") {
@@ -60,60 +60,64 @@ export default function Login() {
       setGeneralError("Server error. Please try again later.");
       console.error(err);
     }
-  }    
-   
+  };
+
   return (
-    <>
-      <div className="min-h-screen bg-[url('/homePage.png')] flex justify-center">
-        <div className="pt-10 w-full flex items-center">
-          <BlueBox className="pr-3 mb-5">
-            <Header text="ALMOST THERE!" className="pl-2" />
-            <div className="pt-5">
-              <InputField
-                id="email"
-                text="Email"
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder="Enter your email"
-              />
-              {emailError && (
-                <p className="text-red-700 text-md mt-1 ml-1">{emailError}</p>
-              )}
-              <InputField
-                id="password"
-                text="Password"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Enter your password"
-              />
-              {passwordError && (
-                <p className="text-red-700 text-md mt-1 ml-1">{passwordError}</p>
-              )}
-              {generalError && (
-                <p className="text-red-700 text-md mt-2 ml-1">{generalError}</p>
-              )}
-              <PrimaryButton
-                text="LOGIN"
-                className="float-right mt-4 ml-4"
-                onClick={handleLogin}
-              />              <div className="flex flex-col items-start">
-                <TextBottom className="pt-4 text-white hover:underline cursor-pointer">
-                  Forget Password?
-                </TextBottom>
-                <TextBottom className="pt-1 text-white hover:underline cursor-pointer" onClick={() => navigate("/login/guest")}>
-                  Login as guest
-                </TextBottom>
-                <p className="pt-1 text-white">
-                  Doesn't have an account yet?
-                </p>
-              </div>
-              <PrimaryButton text="SIGN UP" className="mt-4 bg-gray" onClick={() => navigate("/register")} />
+    <div className="min-h-screen bg-[url('/homePage.png')] flex justify-center">
+      <div className="pt-10 w-full flex items-center">
+        <BlueBox className="pr-3 mb-5">
+          <Header text="ALMOST THERE!" className="pl-2" />
+          <div className="pt-5">
+            <InputField
+              id="email"
+              text="Email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Enter your email"
+            />
+            {emailError && (
+              <p className="text-red-700 text-md mt-1 ml-1">{emailError}</p>
+            )}
+            <InputField
+              id="password"
+              text="Password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter your password"
+            />
+            {passwordError && (
+              <p className="text-red-700 text-md mt-1 ml-1">{passwordError}</p>
+            )}
+            {generalError && (
+              <p className="text-red-700 text-md mt-2 ml-1">{generalError}</p>
+            )}
+            <PrimaryButton
+              text="LOGIN"
+              className="float-right mt-4 ml-4"
+              onClick={handleLogin}
+            />
+            <div className="flex flex-col items-start">
+              <TextBottom className="pt-4 text-white hover:underline cursor-pointer">
+                Forget Password?
+              </TextBottom>
+              <TextBottom
+                className="pt-1 text-white hover:underline cursor-pointer"
+                onClick={() => navigate(LOGIN_GUEST)}
+              >
+                Login as guest
+              </TextBottom>
+              <p className="pt-1 text-white">Doesn't have an account yet?</p>
             </div>
-          </BlueBox>
-        </div>
+            <PrimaryButton
+              text="SIGN UP"
+              className="mt-4 bg-gray"
+              onClick={() => navigate(REGISTER)}
+            />
+          </div>
+        </BlueBox>
       </div>
-    </>
+    </div>
   );
 }
