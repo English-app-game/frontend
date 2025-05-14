c8b2d950282adcfa13c90785c8448054ea2d06cb
LiadLati <liadl57@gmail.com>
2025-05-14 14:25:37 +0300

    userRegistration consts definition complete

--- src/services/service.js
+++ src/services/service.js
@@ -1,5 +1,8 @@
+import { BASE_URL } from "../assets/consts";
+import { REGISTER_PATH } from "../assets/consts";
+
 export async function registerUser(userToSend) {
-  const res = await fetch("http://localhost:5000/api/users/register", {
+  const res = await fetch(`${BASE_URL}${REGISTER_PATH}`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(userToSend),
