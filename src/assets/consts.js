5f1991cb7589063efb16f0b87af8210a17b5c547
maymichaeli <maushe55@gmail.com>
2025-05-23 18:41:43 +0300

    URL change to 5000

--- src/assets/consts.js
+++ src/assets/consts.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:5001/api";
+export const BASE_URL = "http://localhost:5000/api";
 export const REGISTER_PATH = "/users/register";
--- src/services/room/getRooms.js
+++ src/services/room/getRooms.js
@@ -1,8 +1,8 @@
-const ROOMS_API_URL = "http://localhost:5001/api/rooms";
+const ROOMS_API_URL = "http://localhost:5000/api/rooms";
 
 export const fetchRooms = async () => {
   try {
-    const res = await fetch("http://localhost:5001/api/rooms");
+    const res = await fetch("http://localhost:5000/api/rooms");
     if (!res.ok) throw new Error("Failed to fetch rooms");
 
     const data = await res.json();
