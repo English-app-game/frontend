ca334f70a54cad900ffe2b497fb767b5762bb2ab
LaNguAx <itayakni@gmail.com>
2025-05-30 12:34:46 +0300

    fixes-1

--- src/consts/consts.js
+++ src/consts/consts.js
@@ -2,3 +2,4 @@ export const BASE_URL = "http://localhost:5000/api";
 export const REGISTER_PATH = "/users/register";
 export const RESET_PASSWORD_PATH = "/users/login/resetPassword";
 export const SET_NEW_PASSWORD_PATH = "/users/resetPassword";
+export const GET_PLAYERS_ROUTE = `${BASE_URL}/rooms/players`
--- src/services/room/getPlayers.js
+++ src/services/room/getPlayers.js
@@ -1,6 +1,8 @@
+import { GET_PLAYERS_ROUTE } from "../../consts/consts";
+
 export const fetchPlayers = async (roomKey) => {
   try {
-    const res = await fetch(`http://localhost:5000/api/rooms/players/${roomKey}`);
+    const res = await fetch(`${GET_PLAYERS_ROUTE}/${roomKey}`);
     if (!res.ok) throw new Error("Failed to fetch players");
 
     const data = await res.json();
