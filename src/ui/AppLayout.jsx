70e80e2db9ad7a2f2cb24acb72ce7f3ab4e4bec0
maymichaeli <maushe55@gmail.com>
2025-06-04 23:39:28 +0300

    add consts to AppLayout

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,18 +1,25 @@
 import { Outlet, useLocation } from "react-router-dom";
 import UserInfoHeader from "./components/UserInfoHeader";
+import {
+  HOME,
+  LOGIN,
+  LOGIN_GUEST,
+  REGISTER,
+  RESET_PASSWORD,
+  SET_NEW_PASSWORD,
+} from "../routes/routes_consts";
 
 export default function AppLayout() {
   const location = useLocation();
 
   const hideHeaderOnRoutes = [
-    "/",
-    "/login",
-    "/login/guest",
-    "/register",
-    "/login-guest",
-    "/reset-password",
-    "/set-new-password",
-  ];
+  HOME,
+  LOGIN,
+  LOGIN_GUEST,
+  REGISTER,
+  RESET_PASSWORD,
+  SET_NEW_PASSWORD,
+];
 
   const shouldShowHeader = !hideHeaderOnRoutes.includes(location.pathname);
 
