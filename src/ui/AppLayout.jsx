19c1c98bf82ea9414ac9a85df921806009ac16e9
LiadLati <liadl57@gmail.com>
2025-06-08 20:57:57 +0300

    headerBar responsive in gameRooms and statistics pages

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,11 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import UserInfoHeader from "./components/UserInfoHeader";
 
 export default function AppLayout() {
+  const location = useLocation();
+  const hideInMobile = ["/rooms", "/statistics"].includes(location.pathname);
 
   return (
-      <div className="h-screen w-screen relative">
-      <UserInfoHeader />
+    <div className="h-screen w-screen relative">
+      <div className={`${hideInMobile ? "hidden sm:block" : "block"}`}>
+        <UserInfoHeader />
+      </div>
       <main className="w-full h-full">
         <Outlet />
       </main>
--- src/ui/pages/ServersRoom/Nav.jsx
+++ src/ui/pages/ServersRoom/Nav.jsx
@@ -1,17 +1,54 @@
+import { useState } from "react";
 import { IoGameControllerOutline } from "react-icons/io5";
+import { FiMenu, FiX } from "react-icons/fi"; 
 import Header from "../../components/Header";
 import ButtonHeader from "../../components/ButtonHeader";
 import { STATISTICS } from "../../../routes/routes_consts";
+import UserInfoHeader from "../../components/UserInfoHeader";
 
 export default function Nav() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header className="flex items-center justify-center gap-5 h-16 py-7 sm:py-10 bg-secondary z-50 shadow-md">
-      <Header
-        text="Join a game room"
-        className="text-primary uppercase text-[1.5rem] sm:text-4xl"
-      />
-      <IoGameControllerOutline size={80} />
-      <ButtonHeader navigateTo={STATISTICS} text={"Statistics"}/>
+      <div className="flex items-center justify-center w-full sm:justify-center relative">
+        <div className="flex items-center justify-center gap-2">
+          <Header
+            text="Join a game room"
+            className="text-primary uppercase text-[1.5rem] sm:text-4xl"
+          />
+          <IoGameControllerOutline size={60} className="text-primary" />
+        </div>
+        <button
+          className="sm:hidden text-primary text-3xl ml-1"
+          onClick={() => setIsOpen(true)}
+        >
+          <FiMenu />
+        </button>
+        <div className="hidden sm:flex items-center left-8 top-4">
+          <ButtonHeader navigateTo={STATISTICS} text="Statistics" />
+        </div>
+      </div>
+      <div
+        className={`fixed top-0 right-0 h-65 w-60 bg-secondary shadow-lg p-6 z-[100] transform transition-transform duration-300 ease-in-out ${
+          isOpen ? "translate-x-0" : "translate-x-full"
+        } sm:hidden border border-primary rounded-xl`}
+      >
+        <button
+          className="text-primary text-3xl mb-6 ml-auto"
+          onClick={() => setIsOpen(false)}
+        >
+          <FiX />
+        </button>
+        <div className="flex flex-col items-end gap-4">
+          <UserInfoHeader isInsideSidebar={true} />
+          <ButtonHeader
+            navigateTo={STATISTICS}
+            text="Statistics"
+            isInsideSidebar={true}
+          />
+        </div>
+      </div>
     </header>
   );
 }
