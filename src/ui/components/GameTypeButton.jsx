b6e2927ff8ab88f6f67ae228d78a985691e5f65e
tomergol15 <golod505@gmail.com>
2025-06-19 22:32:59 +0300

    Update for now -> mayber will change

--- src/ui/components/GameTypeButton.jsx
+++ src/ui/components/GameTypeButton.jsx
@@ -1,18 +1,39 @@
 import React from 'react';
 import PropTypes from "prop-types";
 
+import memoryIcon      from "../../../public/memoryGame.png";
+import translationIcon from "../../../public/guessTheWord.png";
+import guessIcon       from "../../../public/translation.png";
+
+const icons = {
+  "memory game": memoryIcon,
+  translation: translationIcon,
+  "guess the word": guessIcon,
+};
+
 
 export default function GameTypeButton({ gameType, isSelected, onClick }) {
+  const imgSrc = icons[gameType.name?.toLowerCase()] || memoryIcon;
   return (
     <button
       onClick={() => onClick(gameType._id)}
-      className={`px-4 py-2 rounded-lg font-bold text-white border-4 cursor-pointer transition text-sm ${
-        isSelected
-          ? "bg-blue-500 border-blue-700 ring-4 ring-blue-300 scale-105"
-          : "bg-gray-500 border-gray-300"
-      }`}
+     className={`rounded-xl border-4 overflow-hidden
+        transition transform
+        ${isSelected
+          ? "border-blue-700 ring-4 ring-blue-300 scale-105"
+          : "border-gray-300 hover:scale-105"}
+      `}
+       style={{
+        width: "80px",
+        height: "80px",
+        padding: 0,
+      }}
     >
-      {gameType.name || "Unnamed Game"}
+       <img
+        src={imgSrc}
+        alt={gameType.name}
+       className="w-full h-full object-contain"
+      />
     </button>
   );
 }
