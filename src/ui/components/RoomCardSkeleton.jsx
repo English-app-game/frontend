38709b0803ee080b2a758115ca93702d6da06ed5
LiadLati <liadl57@gmail.com>
2025-06-25 01:08:17 +0300

    imgForEveryGameRoom

--- src/ui/components/RoomCardSkeleton.jsx
+++ src/ui/components/RoomCardSkeleton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const RoomCardSkeleton = () => {
   return (
-    <div className="animate-pulse w-30 mt-6 sm:w-52 h-32 sm:h-52 bg-gradient-to-br from-[#f9f3e6] to-[#e6d6c2] rounded-xl shadow-md" />
+    <div className="animate-pulse w-30 mt-6 sm:w-52 h-32 sm:h-52 bg-gradient-to-br from-[#f9f3e6] to-[#e6d6c2] rounded-2xl shadow-md" />
   )
 }
 
