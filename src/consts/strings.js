8cee0fbe78474004e3944b27e67fedd9ceb83a9e
LiadLati <liadl57@gmail.com>
2025-06-30 18:38:23 +0300

    utilsStrings

--- src/consts/strings.js
+++ src/consts/strings.js
@@ -4,3 +4,6 @@ export const waitingRoomS = "waitingRoom";
 export const tokenS = "token";
 export const errorS = "error";
 export const notFound = "404 not found";
+export const stringS = "string";
+export const serverError = "Server error, please try again later.";
+export const failedToFetch = "Failed to fetch:";
